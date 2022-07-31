a614ceaf3c3379f4f5aef688c48f6cd9eafafb0c
appkumark <appk.kanubuddi@gmail.com>
2022-07-31 13:12:22 +0530

    Algorithm Implementation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] intArray = {2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
+
+        countingSort(intArray, 1, 10);
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println("CountingSort: " + intArray[i]);
+        }
+    }
+
+    public static void countingSort(int[] input, int min, int max){
+
+        int[] countArray = new int[(max - min) + 1];
+
+        for (int i = 0; i < input.length; i++){
+            countArray[input[i] - min]++;
+        }
+
+        int j = 0;
+        for ( int i = min; i <= max; i++){
+            while (countArray[i - min] > 0) {
+                input[j++] = i;
+                countArray[i - min]--;
+            }
+        }
+
     }
 }
